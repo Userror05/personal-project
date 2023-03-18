762a8e3a16312960e5a05c27ab19e960168a005b
Theo-F-o <127985488+Theo-F-o@users.noreply.github.com>
2023-03-18 17:04:22 +0100

    modif jeu

--- src/jeu.cpp
+++ src/jeu.cpp
@@ -4,31 +4,23 @@
 void Jeu :: jeu()
 { 
     bool quit = false;
-    while(quit)
-    {
+    
+    
         SDL_Event events;
         while (SDL_PollEvent(&events)) 
         {
 			if (events.type == SDL_QUIT) quit = true;           // Si l'utilisateur a clique sur la croix de fermeture
 			else if (events.type == SDL_KEYDOWN) 
             {              // Si une touche est enfoncee
+				if(events.key.keysym.scancode==SDL_SCANCODE_F)
                 
-				switch (events.key.keysym.scancode) 
-                {
-				case SDL_SCANCODE_F:
-					while(max>=pui){pui+=0,5;};    // car Y inverse
-					break;
-                case SDL_SCANCODE_Q:
-                    choixfini = true;
-                    break;
-				default: 
-                    break;
+                {quit=true;}
+
+                else  B.ActionJoueur();
 				}
             } 
-        }
+ }
 
-}
-}
 
 
   //tant que F pas presser, on fait ActionJoueur()
@@ -37,9 +29,11 @@ void Jeu :: jeu()
     
 
 
-Void Jeu :: Affichage_txt()
+void Jeu :: Affichage_txt()
 {
     //Tant que F pas presser, on fait ActionJoueurVisuel()
+
+    
 }
 
 
