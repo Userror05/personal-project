c66d1960e299bdcc6db5deb003131135b635232d
Theo-F-o <127985488+Theo-F-o@users.noreply.github.com>
2023-05-01 03:57:50 +0200

    doxygen terminé

--- src/core/gravite.h
+++ src/core/gravite.h
@@ -1,61 +1,193 @@
+/**
+
+    @file Gravite.h
+    @brief Déclaration de la classe Gravite pour gérer la gravité appliquée à une balle.
+    */
+
 #ifndef _GRAVITE
 #define _GRAVITE
 
 #include"balle.h"
 
-class Gravite
-{
+/**
+
+    @class Gravite
+
+    @brief Classe pour gérer la gravité appliquée à une balle.
+
+    La classe Gravite permet de gérer l'effet de la gravité sur une balle en mouvement.
+
+    Elle contient différentes méthodes pour actualiser la position et les paramètres de la balle.
+    */
+    class Gravite
+    {
 
     private:
-   
-    float Power; 
 
-    int Angle;
-    
+    float Power; /*< Force de la gravité. */
+    int Angle; /*< Angle de la direction de la gravité. */
+
     public:
-    
-    //float g = -9.81;
 
+    /**
+        @brief Constructeur par défaut de la classe Gravite.
+        Initialise la force et l'angle de la gravité à zéro.
+        */
+        Gravite();
 
-    Gravite();
-    void ActualiseBalleViaDiv(Balle& b) const;
-    void ActualiseMouv(Balle& b)const;
-    void ActualiseAdri(Balle& b, int n)const;
-    void ActualiseDirections(Balle& b)const;
-    void ClearDirections(Balle& b)const;
-    void SetPow(float pow);
-    void SetAng(int ang);
-    int GetAngle()const;
-    float GetPow()const;
-    double RecupA();
-    double RecupB();
-    double ConversionAng();
-    double ConversionX();
-    double ConversionY();
-    Vecteur& Vitesse(Balle& b);
-    void AffAng();
-    void AffPR();
-    void TestRegression();
+    /**
+        @brief Actualise la position de la balle via la division des forces appliquées.
+        @param b Balle à actualiser.
+        */
+        void ActualiseBalleViaDiv(Balle& b) const;
+
+    /**
+        @brief Actualise le mouvement de la balle en fonction de la gravité.
+        @param b Balle à actualiser.
+        */
+        void ActualiseMouv(Balle& b) const;
+
+    /**
+        @brief Actualise la direction de la balle en fonction de l'angle de la gravité.
+        @param b Balle à actualiser.
+        @param n Numéro de la balle.
+        */
+        void ActualiseAdri(Balle& b, int n) const;
+
+    /**
+        @brief Actualise les directions de la balle.
+        @param b Balle à actualiser.
+        */
+        void ActualiseDirections(Balle& b) const;
+
+    /**
+        @brief Efface les directions de la balle.
+        @param b Balle à actualiser.
+        */
+        void ClearDirections(Balle& b) const;
+
+    /**
+        @brief Modifie la force de la gravité.
+        @param pow réel qui défini la Nouvelle force appliqué a la balle.
+        */
+        void SetPow(float pow);
+
+    /**
+        @brief Modifie l'angle de la gravité.
+        @param ang Nouvel angle de la gravité.
+        */
+        void SetAng(int ang);
+
+    /**
+        @brief Retourne l'angle de la gravité.
+        @return L'angle de la gravité.
+        */
+        int GetAngle() const;
+
+    /**
+        @brief Retourne la force de la gravité.
+        @return La force de la gravité.
+        */
+        float GetPow() const;
+
+    /**
+        @brief Calcule la valeur de A pour la conversion d'angle.
+        @return La valeur de A pour la conversion d'angle.
+        */
+        double RecupA();
+
+    /**
+        @brief Calcule la valeur de B pour la conversion d'angle.
+        @return La valeur de B pour la conversion d'angle.
+        */
+        double RecupB();
+
+    /**
+        @brief Convertit l'angle de la gravité en radians.
+        @return L'angle * converti en radians.
+ */
+double ConversionAng();
+
+/**
+ * @brief Convertit la composante X de la gravité en fonction de l'angle.
+ * 
+ * @return La composante X de la gravité convertie.
+ */
+double ConversionX();
+
+/**
+ * @brief Convertit la composante Y de la gravité en fonction de l'angle.
+ * 
+ * @return La composante Y de la gravité convertie.
+ */
+double ConversionY();
+
+/**
+ * @brief Retourne la vitesse de la balle.
+ * 
+ * @param b Balle à utiliser pour le calcul de la vitesse.
+ * @return La vitesse de la balle.
+ */
+Vecteur& Vitesse(Balle& b);
+
+/**
+ * @brief Affiche l'angle de la gravité.
+ */
+void AffAng();
+
+/**
+ * @brief Affiche la force et l'angle de la gravité.
+ */
+void AffPR();
+
+/**
+ * @brief Effectue un test de régression.
+ */
+void TestRegression();
 
 };
-inline void Gravite :: SetPow(float pow)
-{   Power = pow;}
-inline void Gravite :: SetAng(int ang)
-{   Angle = ang;}
 
-inline float Gravite :: GetPow() const {return Power;}
-inline int Gravite :: GetAngle() const {return Angle;}
+/**
 
+    @brief Modifie la force de la gravité.
+    @param pow Nouvelle force de la gravité.
+    */
+    inline void Gravite::SetPow(float pow)
+    {
+    Power = pow;
+    }
 
+/**
 
+    @brief Modifie l'angle de la gravité.
+    @param ang Nouvel angle de la gravité.
+    */
+    inline void Gravite::SetAng(int ang)
+    {
+    Angle = ang;
+    }
 
+/**
 
+    @brief Retourne la force de la gravité.
+    @return La force de la gravité.
+    */
+    inline float Gravite::GetPow() const
+    {
+    return Power;
+    }
 
+/**
 
-
+    @brief Retourne l'angle de la gravité.
+    @return L'angle de la gravité.
+    */
+    inline int Gravite::GetAngle() const
+    {
+    return Angle;
+    }
 
 #endif
-
 //********************************BackUp************************************************************************
 /*
     void ActualiseDivise(Balle& b) const;
--- src/core/obstacle.h
+++ src/core/obstacle.h
@@ -1,36 +1,75 @@
+/**
+
+    @file obstacle.h
+    @brief Définition de la classe Obstacle
+    */
+
 #ifndef _OBSTACLE_H
 #define _OBSTACLE_H
-#include"cellule.h"
-#include<cassert>
 
-class Obstacle
-{
+#include "cellule.h"
+#include <cassert>
 
+/**
+
+    @class Obstacle
+    @brief Classe représentant un obstacle dans la grille
+    Cette classe représente un obstacle dans la grille, qui est une cellule qui ne peut pas être occupée par un robot.
+    */
+    class Obstacle
+    {
     private:
+    Cellule Obstacle; /**< La cellule qui représente l'obstacle */
 
-    Cellule Obstacle;
+public:
+/**
+* @enum typeOBS
+* @brief Enumération représentant le type d'obstacle
+*
+* Cette énumération représente les différents types d'obstacle possibles :
+* - R : un obstacle rond
+* - F : un obstacle carré
+*/
+enum typeOBS{ R=2, F=1 };
 
-    
-    
 
-    public:
-    enum typeOBS{ R=2, F=1 };
 
-    typeOBS obs;
-    
+typeOBS obs; /**< Le type d'obstacle */
 
-    void SelectType(unsigned int choix);
-    
+/**
+ * @brief Sélectionne le type d'obstacle
+ *
+ * Cette méthode permet de sélectionner le type d'obstacle en fonction d'un choix donné.
+ *
+ * @param choix Le choix du type d'obstacle (0 pour F et 1 pour R)
+ */
+void SelectType(unsigned int choix);
 
-    void SetObstacle(unsigned int x,unsigned int y);
-    
-
-   
-
+/**
+ * @brief Définit la position de l'obstacle dans la grille
+ *
+ * Cette méthode permet de définir la position de l'obstacle dans la grille en fonction de ses coordonnées.
+ *
+ * @param x La coordonnée x de la cellule représentant l'obstacle
+ * @param y La coordonnée y de la cellule représentant l'obstacle
+ */
+void SetObstacle(unsigned int x, unsigned int y);
 
 };
 
-inline void Obstacle :: SelectType(unsigned int choix){ assert(choix==1 || choix==0);if(choix==1)obs=R;else obs=F;   }
+/**
 
+    @brief Sélectionne le type d'obstacle
+    Cette méthode permet de sélectionner le type d'obstacle en fonction d'un choix donné.
+    @param choix Le choix du type d'obstacle (0 pour F et 1 pour R)
+    */
+    inline void Obstacle::SelectType(unsigned int choix)
+    {
+    assert(choix == 1 || choix == 0);
+    if (choix == 1)
+    obs = R;
+    else
+    obs = F;
+    }
 
 #endif
